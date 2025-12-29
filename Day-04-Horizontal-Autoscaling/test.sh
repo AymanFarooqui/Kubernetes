73195ff0f1227d06afc25e2ae6925b3f90f025ca
Moahammed Ayman Farooqui <farooquidev10@gmail.com>
2025-12-29 23:33:10 +0530

    Done with HPA

--- Day-04-Horizontal-Autoscaling/test.sh
+++ Day-04-Horizontal-Autoscaling/test.sh
@@ -1,5 +1,5 @@
 for j in {1..10}; do  
-    for i in {1..1000}; do  
+    for i in {1..100}; do  
         curl -s -o /dev/null -w "%{http_code}\n" http://a5519ee8ee62147a7b7c055673f1ee70-2108986447.us-east-1.elb.amazonaws.com/ &  
     done  
     wait  # Wait for all background curl processes to finish before next iteration
